4ec5a0cd92d66f42d5a3dff204a3085c365660d2
Joseph_Tully <tully4school@gmail.com>
2019-10-18 13:18:28 -0400

    kind of got the code to work for the tabs but now working on the api

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 console.log("we are connected!")
     // evt.preventDefault()
-let tabLinks = document.querySelector(".tabLinks")
-let tabContent = document.querySelector(".tabContent")
+let tabLinks = document.querySelectorAll(".tabLinks")
+let tabContent = document.querySelectorAll(".tabContent")
+
 
 // tablinks.forEach(function(opener) {
 // change.addEventListener("click", openTabs);
@@ -9,9 +10,35 @@ let tabContent = document.querySelector(".tabContent")
 // });
 
 // for (let i = 0;)
+// tabLinks.forEach(function(listener){
+// 	tabLinks.addEventListener("click", function(evt) {
+//     evt.preventDefault()
+//     console.log(evt)
+// })
+// }
+
 tabLinks.forEach(function(listener){
-	tabLinks.addEventListener("click", function(evt) {
-    evt.preventDefault()
-    console.log(evt)
+	listener.addEventListener("click", open, function(evt){
+		evt.preventDefault()
+		console.log(evt)
+	})
 })
+
+//the function to open each tab by removing an active class of another
+
+function open(openTab){
+	let currentBtn = openTab.currentTarget; //found out about currentTarget from https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
+	let coin = currentBtn.dataset.coin; // choosing the data-coin attribute
+//stating that any time i click on another button it removes the class from the current target
+	tabContent.forEach(function(addClass){
+		addClass.classList.remove("active")
+	})
+	tabLinks.forEach(function(removeClass){
+		removeClass.classList.remove("active")
+	})
+//stating that any button i push will add the class of active
+	document.querySelector('#' + coin).classList.add("active")
+
+	currentBtn.classList.add("active")
 }
+
