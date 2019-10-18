538d9db1a0ca9067944e249bd42942cd2d506fc5
Joseph_Tully <tully4school@gmail.com>
2019-10-18 14:20:43 -0400

    was able to successfully add the api key

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log("we are connected!")
     // evt.preventDefault()
 let tabLinks = document.querySelectorAll(".tabLinks")
 let tabContent = document.querySelectorAll(".tabContent")
-
+let url="https://api.nomics.com/v1/exchange-rates?key=684cffb24d974a6e8f18bea5d0e5b926"
 
 // tablinks.forEach(function(opener) {
 // change.addEventListener("click", openTabs);
@@ -16,6 +16,19 @@ let tabContent = document.querySelectorAll(".tabContent")
 //     console.log(evt)
 // })
 // }
+fetch(url)
+.then(res=> {
+	return res.json()
+})
+.then(res => {
+	console.log(res)
+})
+.then(res=> {
+	console.log("success!",res)
+})
+.catch(err => {
+	console.log("NOOOO!!!", err)
+})
 
 tabLinks.forEach(function(listener){
 	listener.addEventListener("click", open, function(evt){
